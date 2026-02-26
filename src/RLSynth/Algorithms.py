Moving average of episodes and fix in stopping calculations

src/RLSynth/Algorithms.py
@@ -90,7 +90,7 @@ class RLOptimizer(object):
 
         if logger is None:
             self.logger = UT.Logger(config['average_type'], 0.99)
-            self.advantage_logger = UT.Logger(config['average_type'], 0.9)
+            self.advantage_logger = UT.Logger(config['average_type'], 0.33)
 
             for log in what2log:
                 self.logger.set_up_log(log)
@@ -102,12 +102,12 @@ class RLOptimizer(object):
                     if len(synth_parameter_object.options) == 0:
                         self.logger.set_up_log(f'{log_name}_diff')
 
-            for t in range(T):
-                self.advantage_logger.set_up_log(f'{self.agent.module_name}_advantage_{t}')
+            # for t in range(T):
+                # self.advantage_logger.set_up_log(f'{self.agent.module_name}_advantage_{t}')
 
-                for level in self.level2agent.keys():
-                    for agent in self.level2agent[level]:
-                        self.advantage_logger.set_up_log(f'{agent.module_name}_advantage_{t}')
+                # for level in self.level2agent.keys():
+                #     for agent in self.level2agent[level]:
+                #         self.advantage_logger.set_up_log(f'{agent.module_name}_advantage_{t}')
 
             for level in self.level2agent.keys():
                 for agent in self.level2agent[level]:
@@ -115,6 +115,7 @@ class RLOptimizer(object):
                     self.logger.set_up_log(f'{agent.module_name}_extremity')
                     self.logger.set_up_log(f'{agent.module_name}_dwell')
                     self.advantage_logger.set_up_log(f'{agent.module_name}_advantage')
+                    self.advantage_logger.set_up_log(f'{agent.module_name}_advantage_std')
 
         else:
             self.logger = logger
@@ -251,7 +252,7 @@ class RLOptimizer(object):
         stopping_loss = torch.zeros(1, device=self.device)
         stopping_regularization = torch.zeros(1, device=self.device)
         if stop_prob is not None:
-            stopping_loss = delta * stop_prob
+            stopping_loss = (delta + self.etta) * stop_prob
             stopping_regularization = torch.abs(0.5 - stop_prob)
 
         # print(f"Stopping loss: {stopping_loss}, Stopping regularization: {stopping_regularization}")
@@ -268,11 +269,13 @@ class RLOptimizer(object):
         loss_weight = self.get_loss_weight(agent)
         self.losses.append(self.get_critic_loss(critic_error) * loss_weight)
         self.losses.append(self.get_critic_lipschitz_reg(critic_td_error, agent, action) * loss_weight)
-        self.advantage_logger.update_average(f'{agent.module_name}_advantage', advantage)
+
+        # Updating the temporary average so all the advantages are equally weighted
+        self.advantage_logger.update_batch_average(f'{agent.module_name}_advantage', advantage)
         advantage_for_stopping = advantage
         if self.normalize_reward and self.logger.loss_calc_step > 10000:
-            advantage = (advantage - self.advantage_logger.averages[f'{agent.module_name}_advantage']) \
-                        / self.advantage_logger.std[f'{agent.module_name}_advantage']
+            std = self.advantage_logger.std[f'{agent.module_name}_advantage_std'] + 1e-8
+            advantage = (advantage - self.advantage_logger.averages[f'{agent.module_name}_advantage']) / std
 
         self.losses.append(self.get_actor_loss(advantage, action_log_prob, transition) * loss_weight)
         self.losses.append(self.get_representation_reg(signal_representation) * loss_weight)
@@ -390,15 +393,16 @@ class RLOptimizer(object):
         reward = torch.tensor(transition.reward)
         critic_error = reward
         advantage = transition.G
+        td_target = transition.G
         if self.is_with_baseline:
             if state1_representation is None or state2_representation is None:
                 state1_representation, state2_representation, _ = self.get_state_representations(critic, transition)
 
             critic_value1 = critic.get_value_of_state(state1_representation)
             self.logger.update_average('critic_value', critic_value1.item())
-            critic_error = transition.G - critic_value1  # Critic error is always w.r.t G[t]
+            critic_error = td_target - critic_value1  # Critic error is always w.r.t G[t]
             # print(transition.G, critic_value1, critic_error, 'G')
-            advantage = critic_error.item()
+            advantage = critic_error.detach()
 
             # TD estimate instead of Monte Carlo estimate
             if self.is_TD:
@@ -410,11 +414,9 @@ class RLOptimizer(object):
 
                 td_target = reward + self.gamma ** transition.n_steps * critic_value2
                 advantage = td_target - critic_value1.item()
-                # print(advantage.item(), reward, critic_value1.item(), critic_value2)
                 critic_error = td_target - critic_value1
-                # print(critic_value1, critic_value2, transition.reward, target)
                 # print(transition.state1[-1], transition.state2[-1])
-        return critic_error, advantage, transition.G
+        return critic_error, advantage, td_target.cpu().numpy()
 
     def get_advantage(self, transition: UT.Transition = None, state1_representation=None, state2_representation=None):
         if transition.advantage is not None:
@@ -443,7 +445,7 @@ class RLOptimizer(object):
             critic_error, critic_td_error, td_target = self.get_critic_error(transition, None, None, self.critic)
 
         self.logger.update_average('td_target', td_target)
-        transition.td_error = abs(critic_td_error)
+        transition.td_error = abs(td_target)
         self.calc_loss_components(agent=sub_agent,
                                   action=transition.action,
                                   advantage=advantage,
@@ -626,6 +628,7 @@ class OnlineAlgorithm(RLOptimizer):
             self.logger.opt_step += 1
             self.update_parameters_wrt_grads()
             self.update_learning_schedulers()
+            self.update_batch_advantage_statistics()
             self.n_steps_for_update = 0
 
         return self.R
@@ -663,6 +666,27 @@ class OnlineAlgorithm(RLOptimizer):
     def update_parameters_wrt_grads(self, delta=1):
         super(OnlineAlgorithm, self).update_parameters_wrt_grads()
         self.critic.copy_weights_to_target()
+
+    def update_batch_advantage_statistics(self):
+        for level in self.level2agent.keys():
+            for agent in self.level2agent[level]:
+                key = f'{agent.module_name}_advantage'
+                if self.advantage_logger.count_for_temporary_average[key] == 0:
+                    continue
+                batch_mean = (self.advantage_logger.batch_averages[key] /
+                                  self.advantage_logger.count_for_temporary_average[key])
+                self.advantage_logger.update_average(key, batch_mean)
+
+                batch_mean_squared = (self.advantage_logger.batch_averages_squared[key] /
+                                      self.advantage_logger.count_for_temporary_average[key])
+
+                batch_std = (batch_mean_squared - batch_mean ** 2) ** 0.5
+                self.advantage_logger.update_average(f'{key}_std', batch_std)
+
+                # Zero the means for the next update
+                self.advantage_logger.batch_averages[key] = 0
+                self.advantage_logger.batch_averages_squared[key] = 0
+                self.advantage_logger.count_for_temporary_average[key] = 0
 
 
 class OfflineAlgorithm(RLOptimizer):

src/RLSynth/Environment.py
@@ -35,7 +35,7 @@ class Environment(object):
     """
     def __init__(self, synth_config, hierarchy_type, is_dynamic_synth: bool, randomize_time=6, device='cpu',
                  reward_type=['similarity'], reward_weight=[1], max_state_val=1, scale_factor=1.0, reward_offset=0,
-                 edge_penalty=0, action_scale='linear'):
+                 starting_tolerance=0.05, ending_tolerance=0.01, edge_penalty=0, action_scale='linear'):
         self.device = device
 
         self.set_synth_related_attributes(synth_config)
@@ -71,7 +71,8 @@ class Environment(object):
         self.reward_offset = reward_offset
         self.edge_penalty = edge_penalty
         self.action_scale = action_scale
-        self.tolerance = 0.05
+        self.tolerance = starting_tolerance
+        self.ending_tolerance = ending_tolerance
 
     def set_synth_related_attributes(self, synth_config):
         self.config = synth_config
@@ -461,7 +462,7 @@ class Environment(object):
 
     def decrease_tolerance(self):
         self.tolerance *= 0.95
-        self.tolerance = max([self.tolerance, 0.01])
+        self.tolerance = max([self.tolerance, self.ending_tolerance])
 
     def save_sound(self, save_type):
         """
@@ -1033,7 +1034,7 @@ class OptionBasedAgent(Agent):
             for i, next_agent in enumerate(self.next_agents):
                 next_agent.load_models(new_path + str(i))
 
-    def check_stopping(self, task_beta, n_trials=0, stopped=False, is_optimal=False):
+    def check_stopping(self, task_beta, n_trials=0, is_optimal=False):
         """
             If we stop sub-agent by chance, we check whether it's the top agent.
             If the agent stopped is not a top agent, we continue to sample next agent in hierarchy
@@ -1041,7 +1042,7 @@ class OptionBasedAgent(Agent):
         sub_task_stopped = False
         if not self.is_top_agent:
             p = np.random.random()
-            task_beta_temp = task_beta.item() if not stopped else torch.ones(1)
+            task_beta_temp = task_beta.item()
             if n_trials <= 2:
                 if (task_beta_temp > 0.5 and is_optimal) or p < task_beta_temp:
                     sub_task_stopped = True
@@ -1131,12 +1132,13 @@ class HierarchicalAgent(Manager, Agent):
         return policy, termination
 
     def search_agent(self, state_representation, t, logger: Utils.Logger = None, is_optimal=False):
-        # if t == 0:
-        #     self.active_agents_indices = list(self.indices2agent_keys[3])
-        #     active_agent = self.get_agent_from_indices(self.current_agent_level)
-        #     active_agent.start_time = 1
-        #     self.top_agent.start_time = 1
-        #     return
+        if t == 0 and self.environment.synth.n_oscillators > 1:
+            self.active_agents_indices = list(self.indices2agent_keys[3])
+            active_agent = self.get_agent_from_indices(self.current_agent_level)
+            active_agent.start_time = 1
+            self.top_agent.start_time = 2
+            active_agent.previous_agent.start_time = 2
+            return
 
         active_agent = self.get_agent_from_indices(self.current_agent_level)
         agent_for_log = active_agent
@@ -1186,8 +1188,10 @@ class HierarchicalAgent(Manager, Agent):
 
             policy, stop_prob, _ = active_agent.get_policy_from_state_representation(state_representation)
             if is_direction_up:
-                sub_task_stopped = active_agent.check_stopping(stop_prob, n_trials,
-                                                               stopped=sub_task_stopped, is_optimal=is_optimal)
+                if sub_task_stopped and len(active_agent.next_agents) == 1:
+                    sub_task_stopped = True
+                else:
+                    sub_task_stopped = active_agent.check_stopping(stop_prob, n_trials, is_optimal=is_optimal)
             agent_for_log = active_agent
 
     def to(self, device):
@@ -1407,7 +1411,7 @@ def create_trajectory(n_actions, episode: Utils.Episode, agent, env: Environment
         plt.imshow(env.current_signal_mel_spec.cpu().numpy())
         plt.show()
 
-    if isinstance(agent, HierarchicalAgent) or isinstance(agent, CyclicAgent) and len(episode.States) <= 1:
+    if isinstance(agent, HierarchicalAgent) or isinstance(agent, CyclicAgent) and len(episode.States) <= 0:
         agent.init_agents()
 
     if show_spec:
@@ -1736,6 +1740,8 @@ def initialize_env(synth_config_dir, environment_config_dir) -> Environment:
                       reward_type=environment_config['reward_type'],
                       reward_weight=environment_config['reward_weight'],
                       reward_offset=environment_config['reward_offset'],
+                      starting_tolerance=environment_config['starting_tolerance'],
+                      ending_tolerance=environment_config['ending_tolerance'],
                       edge_penalty=environment_config['edge_penalty'],
                       action_scale=environment_config['action_scale'],
                       max_state_val=max_state_val,

src/RLSynth/Training/Training_Scripts/Training.py
@@ -28,12 +28,13 @@ with open(model_config_file) as f:
 with open(training_config_file) as f:
     training_config = dict(json.load(f))
     device = training_config["device"]
-    n_steps = training_config["n_steps"]
-    T = training_config["episode_len"]
+    T_list = training_config['T_list']
+    n_steps_list = training_config['n_steps_list']
     lr_agent_representation = training_config["lr_agent_representation"]
     lr_critic_representation = training_config["lr_critic_representation"]
     lr_agent = training_config["lr_agent"]
     lr_critic = training_config["lr_critic"]
+    lr_level_multiplier = training_config["lr_level_multiplier"]
     exploratory = training_config["exploratory"]
     baseline = training_config["baseline"]
     algorithm = training_config["algorithm"]
@@ -65,15 +66,14 @@ level_to_model = Environment.DFS_agent_parameters(manager.top_agent)
 n_levels = len(level_to_model.keys()) - 1
 for level, agents_models in level_to_model.items():
     for model, model_name, sub_agent in agents_models:
-        multiplier = 0.1 if len(sub_agent.next_agents) > 0 else 1
-        # multiplier = 1
+        multiplier = lr_level_multiplier if len(sub_agent.next_agents) > 0 else 1
         # Higher learning rate for categorical choices
-        # if len(sub_agent.next_agents) == 0 and len(sub_agent.options) > 0:
-        #     multiplier *= 10
+        if len(sub_agent.next_agents) == 0 and len(sub_agent.options) > 0:
+            multiplier *= 2.5
 
         for sub_model_name, sub_model in model.named_children():
-            if sub_model_name == 'termination':
-                multiplier *= 0.1
+            if sub_model_name == 'termination' and len(sub_agent.options) == 0:
+                multiplier *= 0.2
                 # multiplier *= 1
 
             param_dict = {'params': list(sub_model.parameters()), 'lr': lr_agent * multiplier}
@@ -91,23 +91,12 @@ for model, name in zip(critic_models, critic_model_names):
 critic.model_parameters = critic_param_list
 
 if model_config["pretrained"]:
-    sound_path_pretrained = fr"{project_dir}/Models/VF/cnn_vf_pretrain.pth"
-    mlp_path_pretrained = fr"{project_dir}/Models/VF/mlp_vf_pretrain.pth"
+    pretrained_experiment_num = model_config["experiment_num"]
+    algorithm_experiment = model_config["algorithm"]
+    manager.load_models(fr'{project_dir}/TrainedModels/{algorithm_experiment}/{pretrained_experiment_num}/Agent')
+    critic.load_models(fr'{project_dir}/TrainedModels/{algorithm_experiment}/{pretrained_experiment_num}/Critic')
     # sound_path_pretrained = fr"C:/Users/sgvfe/Desktop/Degree/2nd_degree/Thesis/Models/DeltaLab/enc_dec.ckpt"
     # parameters_path_pretrained = fr"C:/Users/sgvfe/Desktop/Degree/2nd_degree/Thesis/Models/SST/ParametersEncoder.pth"
-
-    sound_model = Models.SimpleCNN([16, 16, 16], [3, 3, 3], representation_dim, env.n_frames, 128)
-    state_dict = torch.load(sound_path_pretrained)
-    sound_model.load_state_dict(state_dict)
-
-    if model_config['are_shared_mlp']:
-        mlp = Models.RLMLP(representation_dim, 1, env.curr_synth_parameters.shape[0], models_dim, n_layers)
-        state_dict = torch.load(mlp_path_pretrained)
-        mlp.load_state_dict(state_dict)
-        manager.advanced_representation = mlp
-
-    manager.SoundEncoder = sound_model
-    critic.SoundEncoder = sound_model
 
 manager.to(device=device)
 critic.to(device=device)
@@ -159,13 +148,8 @@ R = list()
 run = None
 logger = None
 advantage_logger = None
-T = 30
 
-for T, n_steps in zip([30, 35], [15000, 15000]):
-# for T, n_modules, n_parameters, n_steps in zip([30, 30, 30, 40, 50, 60],
-#                                     [1, 1, 2, 2, 2, 2],
-#                                     [1, 2, 1, 2, 2, 2],
-#                                     [1000, 1000, 10000, 10000, 10000, 10000]):
+for T, n_steps in zip(T_list, n_steps_list):
     RL_optimizer = Algorithms.algorithm2class[algorithm](optimizer_agent=optimizer_agent,
                                                          optimizer_critic=optimizer_critic,
                                                          lr_scheduler_agent=lr_scheduler_agent,
@@ -195,26 +179,6 @@ pr.enable()
 pr.disable()
 pr.dump_stats('profile.pstat')
 
-# for i in range(2, n_levels + 1):
-#     with open(fr'Level{i}_Synth.json') as f:
-#         synth_config2 = dict(json.load(f))
-#         agent.level_up(synth_config2)
-#
-#     if training_config["baseline"] or algorithm == "ActorCritic":
-#         critic.level_up(synth_config)
-#     RL_optimizer = Algorithms.algorithm2class[algorithm](optimizer_agent=optimizer_agent,
-#                                                          optimizer_critic=optimizer_critic,
-#                                                          lr_scheduler_agent=lr_scheduler_agent,
-#                                                          lr_scheduler_critic=lr_scheduler_critic,
-#                                                          env=env,
-#                                                          agent=agent,
-#                                                          critic=critic,
-#                                                          n_steps=n_steps[1],
-#                                                          T=T[1],
-#                                                          config=training_config)
-#
-#     R.append(RL_optimizer.optimize())
-
 if not os.path.isdir(fr"{project_dir}/TrainedModels/{algorithm}"):
     os.mkdir(fr"{project_dir}/TrainedModels/{algorithm}")
 
@@ -236,6 +200,3 @@ if training_config["baseline"] or algorithm == 'ActorCritic' or algorithm == 'Op
     critic_dir = fr"{project_dir}/TrainedModels/{algorithm}/{experiment_num}/Critic"
     os.mkdir(critic_dir)
     critic.save_models(critic_dir)
-
-with open(training_config_file, 'wt') as f:
-    json.dump(training_config, f)

src/RLSynth/Utils.py
@@ -150,12 +150,20 @@ class Logger(object):
         self.averages_squared = dict()
         self.std = dict()
         self.count_for_average = dict()
+        self.batch_averages = dict()
+        self.batch_averages_squared = dict()
+        self.count_for_temporary_average = dict()
 
         self.what2log_t = dict()
         self.what2log = dict()
         self.step = 0
         self.loss_calc_step = 0
         self.opt_step = 0
+
+    def update_batch_average(self, average, new_value):
+        self.batch_averages[average] += new_value
+        self.batch_averages_squared[average] += new_value ** 2
+        self.count_for_temporary_average[average] += 1
 
     # Update average of a specific logged variable
     def update_average(self, average, new_value, add_to_count=True):
@@ -184,16 +192,16 @@ class Logger(object):
                                  (1 - self.moving_avg_alpha) * new_value
         self.averages_squared[average] = (1 - self.moving_avg_alpha) * (self.averages_squared[average] +
                                                                         self.moving_avg_alpha * np.power(diff_from_mean1, 2))
-        # self.averages_squared[average] = self.moving_avg_alpha * self.averages_squared[average] +\
-        #                                  (1 - self.moving_avg_alpha) * np.power(diff_from_mean1, 2)
-        # self.std[average] = np.sqrt(self.averages_squared[average]) / self.count_for_average[average]
         self.std[average] = np.sqrt(self.averages_squared[average])
 
     def set_up_log(self, what2log):
         self.averages[what2log] = 0
         self.averages_squared[what2log] = 0
+        self.batch_averages[what2log] = 0
+        self.batch_averages_squared[what2log] = 0
         self.std[what2log] = 1
         self.count_for_average[what2log] = 0
+        self.count_for_temporary_average[what2log] = 0
 
 
 class PPOMemory(Dataset):